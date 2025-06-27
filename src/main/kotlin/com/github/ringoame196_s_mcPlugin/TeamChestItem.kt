9c60df0cc00b2e756782def165a18847205f93e4
りんご飴196 <kazumac41@gmail.com>
2025-06-28 02:45:54 +0900

    ♻️ リファクタリング

--- src/main/kotlin/com/github/ringoame196_s_mcPlugin/TeamChestItem.kt
+++ src/main/kotlin/com/github/ringoame196_s_mcPlugin/TeamChestItem.kt
@@ -3,6 +3,6 @@ package com.github.ringoame196_s_mcPlugin
 import org.bukkit.inventory.ItemStack
 
 data class TeamChestItem(
-	val slot: Int,
-	val item: ItemStack
+    val slot: Int,
+    val item: ItemStack
 )
