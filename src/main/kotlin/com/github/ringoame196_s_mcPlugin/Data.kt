517548ab5b193ffd6a11dfa3fdabee19d0db9faa
りんご飴196 <kazumac41@gmail.com>
2025-06-28 18:25:55 +0900

    🐛 https://github.com/ringoame196-s-mcPlugin/TeamChest/issues/7 DBファイルのパス関係のエラー対処 DataにdataFolderPathを使って回避

---
+++ src/main/kotlin/com/github/ringoame196_s_mcPlugin/Data.kt
@@ -0,0 +1,5 @@
+package com.github.ringoame196_s_mcPlugin
+
+object Data {
+    lateinit var dataFolderPath: String
+}
--- src/main/kotlin/com/github/ringoame196_s_mcPlugin/Main.kt
+++ src/main/kotlin/com/github/ringoame196_s_mcPlugin/Main.kt
@@ -1,21 +1,17 @@
 package com.github.ringoame196_s_mcPlugin
 
 import com.github.ringoame196_s_mcPlugin.commands.Command
-import com.github.ringoame196_s_mcPlugin.events.Events
+import com.github.ringoame196_s_mcPlugin.events.TeamChestEvent
 import org.bukkit.plugin.java.JavaPlugin
 
 class Main : JavaPlugin() {
     private val plugin = this
 
-    companion object {
-        lateinit var instance: Main
-            private set
-    }
-
     override fun onEnable() {
         super.onEnable()
         saveResource("data.db", false)
-        server.pluginManager.registerEvents(Events(), plugin)
+        Data.dataFolderPath = plugin.dataFolder.path
+        server.pluginManager.registerEvents(TeamChestEvent(), plugin)
         val command = getCommand("teamchest")
         command!!.setExecutor(Command())
     }
