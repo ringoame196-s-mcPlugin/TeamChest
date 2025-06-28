dd1c929a8eef97be39a3aff41b54fc6c2638d58e
りんご飴196 <kazumac41@gmail.com>
2025-06-28 20:16:13 +0900

    ✨ 効果音追加 https://github.com/ringoame196-s-mcPlugin/TeamChest/issues/10

--- src/main/kotlin/com/github/ringoame196_s_mcPlugin/TeamChestManager.kt
+++ src/main/kotlin/com/github/ringoame196_s_mcPlugin/TeamChestManager.kt
@@ -2,6 +2,7 @@ package com.github.ringoame196_s_mcPlugin
 
 import org.bukkit.Bukkit
 import org.bukkit.ChatColor
+import org.bukkit.Sound
 import org.bukkit.entity.Player
 import org.bukkit.inventory.Inventory
 import org.bukkit.scoreboard.Team
@@ -18,14 +19,18 @@ object TeamChestManager {
     fun openTeamChest(player: Player, team: Team) {
         val teamName = team.name
         val teamChest = getTeamChest(teamName)
+        val sound = Sound.BLOCK_CHEST_OPEN
         playerOpenedTeamChest[player.uniqueId] = teamName
         player.openInventory(teamChest)
+        player.playSound(player, sound, 1f, 1f)
         additionView(teamName)
     }
 
     fun closeTeamChest(player: Player) {
         val teamName = playerOpenedTeamChest[player.uniqueId] ?: return
+        val sound = Sound.BLOCK_CHEST_CLOSE
         playerOpenedTeamChest.remove(player.uniqueId)
+        player.playSound(player, sound, 1f, 1f)
         reduceView(teamName)
         if (teamChestViewers[teamName] == 0) {
             updateTeamChest(teamName)
