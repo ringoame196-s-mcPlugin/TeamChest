e40390ecf7738dc884c0435a8613d45a0d774c77
りんご飴196 <kazumac41@gmail.com>
2025-06-28 13:55:01 +0900

    ✨ plugin companion object

--- src/main/kotlin/com/github/ringoame196_s_mcPlugin/Main.kt
+++ src/main/kotlin/com/github/ringoame196_s_mcPlugin/Main.kt
@@ -6,8 +6,15 @@ import org.bukkit.plugin.java.JavaPlugin
 
 class Main : JavaPlugin() {
     private val plugin = this
+
+    companion object {
+        lateinit var instance: Main
+            private set
+    }
+
     override fun onEnable() {
         super.onEnable()
+        saveResource("data.db", false)
         server.pluginManager.registerEvents(Events(), plugin)
         val command = getCommand("teamchest")
         command!!.setExecutor(Command())
